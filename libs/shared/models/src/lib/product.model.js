18a0510e637bb3f69037c7c09c134918f1ce85b5
samiksha.indalkar <samiksha.indalkar@softtech-engr.com>
2026-01-01 12:20:34 +0530

    Navbar file created.

---
+++ libs/shared/models/src/lib/product.model.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../Common/Navbar';
+
+export { Navbar };
+
+function ProjectCore() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to Project!</h1>
+    </div>
+  );
+}
+
+export default ProjectCore;
